59d9448f739acd6d1f998140c32e758122cac832
Zhe <xu.zhe3@husky.neu.edu>
2019-03-31 21:51:20 -0400

    explore map

--- config-overrides.js
+++ config-overrides.js
@@ -1,11 +1,16 @@
-const { injectBabelPlugin } = require('react-app-rewired')
-const rewireLess = require('react-app-rewire-less')
-
-module.exports = function override(config, env) {
-  config = injectBabelPlugin(['import', { libraryName: 'antd', libraryDirectory: 'es', style: true }], config)
-  config = rewireLess.withLoaderOptions({
-    modifyVars: {},
-    javascriptEnabled: true
-  })(config, env)
-  return config
-}
+const {
+    override,
+    fixBabelImports,
+    addLessLoader,
+  } = require("customize-cra");
+  
+  
+  module.exports = override(
+    fixBabelImports("import", {
+      libraryName: "antd", libraryDirectory: "es", style: true // change importing css to less
+    }),
+    addLessLoader({
+      javascriptEnabled: true,
+      modifyVars: { "@primary-color": "#1DA57A" }
+    })
+  );
