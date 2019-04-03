ba8df931831ed0306ab3000d120335b621ab11cb
Zhe <xu.zhe3@husky.neu.edu>
2019-04-02 23:07:21 -0400

    fix bugs and imporve first page reagion click

--- src/redux/action/cityInfo.js
+++
@@ -1,6 +0,0 @@
-export function getCityInfo(geoid) {
-  return {
-    type: 'QUERY_INFO',
-    payload: { geoid }
-  }
-}
--- src/redux/action/queryInfo.js
+++
@@ -1,6 +0,0 @@
-export function setQueryInfo(zip, geoid, pos) {
-  return {
-    type: 'SET_QUERY',
-    payload: { zip, geoid, pos }
-  }
-}
