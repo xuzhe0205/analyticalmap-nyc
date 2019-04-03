a5a6a5c2c22193b3d767edd102372e822705de14
Zhe <xu.zhe3@husky.neu.edu>
2019-04-03 19:38:53 -0400

    enhanced detail page and more polishment

--- src/components/CustomHeader/index.jsx
+++ src/components/CustomHeader/index.jsx
@@ -3,6 +3,6 @@ import './index.less'
 
 export default class CustomHeader extends Component {
   render() {
-    return <div className="custom-header">Logo here</div>
+    return <div className="custom-header">NEW YORK CITY ANALYTICAL MAP</div>
   }
 }
